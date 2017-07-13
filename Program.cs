ce00e0dc9dbe514e16bc6612e92f41a648273e1f
Lucy <lucylaurentino@gmail.com>
2017-07-13 16:20:55 +1200

    Inicial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+            Console.WriteLine("Hello you!"); 
+        }
+    }
+}
